0602d3b2fc59f85a3ea958c99c401d455af320bd
cristian <phaser@tubatuba.net>
2019-03-16 12:30:16 -0600

    sound

--- basketcrow.js
+++ basketcrow.js
@@ -165,8 +165,12 @@ function tick() {
             time_remaining=time_remaining-delta_time;
         } else if(game==false) {
             game=true;
+            audio_horn.play();
+
             setTimeout(function(){
                 scene=0;
+                audio_ambient0.pause();
+                audio_ambient1.pause();
             },6000)
         }
         last_time=Date.now();
@@ -215,6 +219,7 @@ function tick() {
                         crows[1].ball=false;
                         point=i;                
                         scores[i]+=2;
+                        audio_point_cheer.play();
                         setTimeout(function(){
                             reset_crow_position();
                         },1000)
@@ -255,6 +260,8 @@ window.addEventListener('keydown',function(e){
     } else if(scene==2) {
         scene=3;
         audio_music0.pause();
+        audio_ambient0.play();
+        audio_ambient1.play();
         setTimeout(function(){
             reset_crow_position();
 
